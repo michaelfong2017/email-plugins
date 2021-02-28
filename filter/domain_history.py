20bb202ea635a829b07d67424a2bc90e3b0b0946
michaelfong2017 <michaelfong2017@gmail.com>
2021-02-28 18:46:50 +0800

    correct directory

--- filter/domain_history.py
+++ filter/domain_history.py
@@ -8,6 +8,6 @@ INBOX_DIR = os.path.join(USER_DIR, 'cur')
 NEW_USER_DIR = os.path.join(USER_DIR, '.INBOX.New Sender', 'cur')
 
 for filename in os.listdir(INBOX_DIR):
-    filepath = os.path.join(NEW_USER_DIR, filename)
+    filepath = os.path.join(INBOX_DIR, filename)
     print(filepath)
     print(os.stat(filepath).st_mtime)
