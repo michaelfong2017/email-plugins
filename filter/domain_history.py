ea692f88348b97a83f041c6a2604b4b231e2971e
michaelfong2017 <michaelfong2017@gmail.com>
2021-02-28 19:17:46 +0800

    print file

--- filter/domain_history.py
+++ filter/domain_history.py
@@ -1,13 +1,22 @@
 import os
+import time
 
-from time import sleep
 from daemonize import Daemonize
 
 USER_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 INBOX_DIR = os.path.join(USER_DIR, 'cur')
 NEW_USER_DIR = os.path.join(USER_DIR, '.INBOX.New Sender', 'cur')
 
+## Control which emails (files) to check
+CHECK_ALL_EMAILS = True
+CHECK_EMAILS_MODIFIED_WITHIN = 20 # Check all emails that are last modified within t seconds
+
 for filename in os.listdir(INBOX_DIR):
     filepath = os.path.join(INBOX_DIR, filename)
-    print(filepath)
-    print(os.stat(filepath).st_mtime)
+    mtime = os.stat(filepath).st_mtime
+
+    if CHECK_ALL_EMAILS == True or time.time() - mtime < CHECK_EMAILS_MODIFIED_WITHIN: # Check this email (file)
+
+        with open(filepath, "r") as f:
+            for line in f.readlines():
+                print(line)
