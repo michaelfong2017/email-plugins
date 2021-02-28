5b3c50bf9b190c2162e3dba4c65d35790b65e33e
michaelfong2017 <michaelfong2017@gmail.com>
2021-02-28 18:17:27 +0800

    Test listdir

---
+++ filter/domain_history.py
@@ -0,0 +1,10 @@
+import os
+
+from time import sleep
+from daemonize import Daemonize
+
+USER_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+INBOX_DIR = os.path.join(USER_DIR, 'cur')
+NEW_USER_DIR = os.path.join(USER_DIR, '".INBOX.New Sender"')
+
+print(os.listdir(NEW_USER_DIR))
