ab1b194a181ea99fbfc90efaa8e6c263903aac34
michaelfong2017 <michaelfong2017@gmail.com>
2021-02-28 20:28:46 +0800

    print domain_list and sender

--- filter/domain_history.py
+++ filter/domain_history.py
@@ -8,10 +8,15 @@ from daemonize import Daemonize
 USER_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 INBOX_DIR = os.path.join(USER_DIR, 'cur')
 NEW_USER_DIR = os.path.join(USER_DIR, '.INBOX.New Sender', 'cur')
+DOMAIN_FILE = "domain.txt"
 
 ## Control which emails (files) to check
 CHECK_ALL_EMAILS = True
 CHECK_EMAILS_MODIFIED_WITHIN = 20 # Check all emails that are last modified within t seconds
+
+with open(DOMAIN_FILE, "r") as f:
+    domain_list = f.readlines()
+print(domain_list)
 
 for filename in os.listdir(INBOX_DIR):
     filepath = os.path.join(INBOX_DIR, filename)
@@ -20,11 +25,18 @@ for filename in os.listdir(INBOX_DIR):
     if CHECK_ALL_EMAILS == True or time.time() - mtime < CHECK_EMAILS_MODIFIED_WITHIN: # Check this email (file)
 
         with open(filepath, "r") as f:
-            msg = email.message_from_file(f)
-            print(msg)
+            msg = email.message_from_file(f) # Whole email message including both headers and content
 
             parser = email.parser.HeaderParser()
             headers = parser.parsestr(msg.as_string())
 
-            for h in headers.items():
-                print(h)
+            # for h in headers.items(): # Get all header information, this can be commented since we now just need to identify the sender
+            #     print(h)
+
+            sender = headers['From']
+
+            print(sender)
+
+            if sender not in domain_list:
+                # Move the email from Inbox mailbox to New Sender mailbox
+                print("not in")
