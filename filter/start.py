1598cc9c1c06cdeeebe4c8d49606dd8739e58af7
michaelfong2017 <michaelfong2017@gmail.com>
2021-03-02 14:32:27 +0800

    subprocess fix

--- filter/start.py
+++ filter/start.py
@@ -17,4 +17,4 @@ subprocess.run(['python3', '-m', 'venv', 'env'])
 if platform.system() == 'Linux' or platform.system() == 'Darwin':
     subprocess.call("env/bin/pip3 install daemonize", shell=True) # subprocess.call waits this subprocess to return before proceeding
     subprocess.call("env/bin/pip3 install mail-parser", shell=True) # subprocess.call waits this subprocess to return before proceeding
-    subprocess.Popen(["env/bin/python3", f"domain_history.py{debug_arg}{sleep_arg}"]) # subprocess.Popen does not wait this subprocess to return before proceeding
+    subprocess.Popen("env/bin/python3 domain_history.py{debug_arg}{sleep_arg}", shell=True) # subprocess.Popen does not wait this subprocess to return before proceeding
