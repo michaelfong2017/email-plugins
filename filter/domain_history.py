5aa315b21df717d095bb01f27a0e11eef494dee6
michaelfong2017 <michaelfong2017@gmail.com>
2021-03-02 14:38:45 +0800

    type=int for argparse

--- filter/domain_history.py
+++ filter/domain_history.py
@@ -13,7 +13,7 @@ from daemonize import Daemonize
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--debug', action='store_true', help='Debug level set from logging.WARNING to logging.DEBUG')
-parser.add_argument('-s', '--sleep', help='Daemon process sleep duration (in seconds) between loops')
+parser.add_argument('-s', '--sleep', type=int, help='Daemon process sleep duration (in seconds) between loops')
 args = parser.parse_args()
 
 
