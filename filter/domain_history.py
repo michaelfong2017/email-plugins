9d7b008db61e01d0d50adba6447b29b2e1a28cd2
michaelfong2017 <michaelfong2017@gmail.com>
2021-03-02 09:26:33 +0800

    Try daemonize

--- filter/domain_history.py
+++ filter/domain_history.py
@@ -19,38 +19,49 @@ DOMAIN_FILE = "domain.txt"
 CHECK_ALL_EMAILS = True
 CHECK_EMAILS_MODIFIED_WITHIN = 20 # Check all emails that are last modified within t seconds
 
-with open(DOMAIN_FILE, "r") as f:
-    domain_list = f.read().splitlines()
-print(f"Recognized domain list from {DOMAIN_FILE}: {domain_list}")
+PID = "process.pid"
 
-for filename in os.listdir(INBOX_DIR):
-    filepath = os.path.join(INBOX_DIR, filename)
-    mtime = os.stat(filepath).st_mtime
+def main():
+    while True:
+        with open(DOMAIN_FILE, "r") as f:
+            domain_list = f.read().splitlines()
+        print(f"Recognized domain list from {DOMAIN_FILE}: {domain_list}")
 
-    if CHECK_ALL_EMAILS == True or time.time() - mtime < CHECK_EMAILS_MODIFIED_WITHIN: # Check this email (file)
+        for filename in os.listdir(INBOX_DIR):
+            filepath = os.path.join(INBOX_DIR, filename)
+            mtime = os.stat(filepath).st_mtime
 
-        print(f"Checking email {filename}")
+            if CHECK_ALL_EMAILS == True or time.time() - mtime < CHECK_EMAILS_MODIFIED_WITHIN: # Check this email (file)
 
-        with open(filepath, "r") as f:
-            msg = email.message_from_file(f) # Whole email message including both headers and content
+                print(f"Checking email {filename}")
 
-            parser = email.parser.HeaderParser()
-            headers = parser.parsestr(msg.as_string())
+                with open(filepath, "r") as f:
+                    msg = email.message_from_file(f) # Whole email message including both headers and content
 
-            # for h in headers.items(): # Get all header information, this can be commented since we now just need to identify the sender
-            #     print(h)
+                    parser = email.parser.HeaderParser()
+                    headers = parser.parsestr(msg.as_string())
 
-            sender = headers['From']
+                    # for h in headers.items(): # Get all header information, this can be commented since we now just need to identify the sender
+                    #     print(h)
 
-            m = re.search(r"\<(.*?)\>", sender) # In case sender is something like '"Chan, Tai Man" <ctm@gmail.com>' instead of 'ctm@gmail.com'
-            if m != None:
-                sender = m.group(1)
+                    sender = headers['From']
 
-            print(f"From: {sender}")
+                    m = re.search(r"\<(.*?)\>", sender) # In case sender is something like '"Chan, Tai Man" <ctm@gmail.com>' instead of 'ctm@gmail.com'
+                    if m != None:
+                        sender = m.group(1)
 
-            if sender not in domain_list:
-                # Move the email from Inbox mailbox to New Sender mailbox
-                print(f"Sender address not recognized, now moving email from {INBOX_MAILBOX} to {NEW_SENDER_MAILBOX}")
-                shutil.move(filepath, os.path.join(NEW_SENDER_DIR, filename))
-            else:
-                print("Sender address recognized")
+                    print(f"From: {sender}")
+
+                    if sender not in domain_list:
+                        # Move the email from Inbox mailbox to New Sender mailbox
+                        print(f"Sender address not recognized, now moving email from {INBOX_MAILBOX} to {NEW_SENDER_MAILBOX}")
+                        shutil.move(filepath, os.path.join(NEW_SENDER_DIR, filename))
+                    else:
+                        print("Sender address recognized")
+
+        sleep(5)
+
+daemon = Daemonize(app="domain_history", pid=PID, action=main)
+daemon.start()
+
+
