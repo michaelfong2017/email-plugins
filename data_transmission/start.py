7623409b756fc8e7dbac3142f78a7c7a20fbe435
michaelfong2017 <michaelfong2017@gmail.com>
2021-03-23 16:41:12 +0800

    also install wheel

--- data_transmission/start.py
+++ data_transmission/start.py
@@ -3,6 +3,7 @@ import platform
 import os
 subprocess.run(['python3', '-m', 'venv', 'env'])
 if platform.system() == 'Linux' or platform.system() == 'Darwin':
+    subprocess.run("env/bin/pip3 install wheel", shell=True) # subprocess.run / subprocess.call (old) waits this subprocess to return before proceeding
     subprocess.run("env/bin/pip3 install grpcio", shell=True) # subprocess.run / subprocess.call (old) waits this subprocess to return before proceeding
     subprocess.run("env/bin/pip3 install grpcio-tools", shell=True) # subprocess.run / subprocess.call (old) waits this subprocess to return before proceeding
     subprocess.Popen(f"env/bin/python3 server.py", shell=True) # subprocess.Popen does not wait this subprocess to return before proceeding
