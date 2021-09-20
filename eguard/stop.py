9f0887ad48f6fa6548c00cd378019626fb49138e
Debian <admin@ip-172-31-15-37.ap-east-1.compute.internal>
2021-09-20 07:59:30 +0000

    1. Fix the crash when subject is None \n 2. Codes to migrate old banners to new banners \n 3. rm .process.pid in stop.py

--- eguard/stop.py
+++ eguard/stop.py
@@ -3,3 +3,4 @@ import platform
 import os
 if platform.system() == 'Linux' or platform.system() == 'Darwin':
     subprocess.run("kill -9 `pgrep -f domain_history.py`", shell=True)
+    subprocess.run("rm .process.pid", shell=True)
--- eguard/util/message_util.py
+++ eguard/util/message_util.py
@@ -11,26 +11,61 @@ import re
 import os
 import shutil
 
+########
+# BEGIN
+# Current warnings
+########
 UNKNOWN_SUBJECT = '''[FROM NEW SENDER] '''
-
 JUNK_SUBJECT = '''[JUNK MAIL] '''
+UNKNOWN_BANNER_HTML = '''<p style="margin: 10px 20%; text-align: center; border: 2px solid; background-color: #ff7400; padding: 5px;"><span>注意：這是首次接收到的電郵地址。除非您確保其真確性，否則請留意當中所附有的超連結，附件或銀行帳戶資料。如有疑問，請尋求技術人員的支援。</span><br />Caution: The domain of email sender is first seen. &nbsp;Beware of any hyperlink, attachment and bank account information unless you ensure the authenticity of the sender. &nbsp;Seek IT assistance if in doubt.&nbsp;&nbsp;</p>\n'''
+JUNK_BANNER_HTML = '''<p style="margin: 10px 20%; text-align: center; border: 2px solid; background-color: #ff4444; padding: 5px;"><span>注意：這是曾被舉報為垃圾發件人的電郵地址。除非您確保其真確性，否則請留意當中所附有的超連結，附件或銀行帳戶資料。如有疑問，請尋求技術人員的支援。</span><br />Warning: The domain of email sender was reported as a junk sender. &nbsp;Beware of any hyperlink, attachment and bank account information unless you ensure the authenticity of the sender. &nbsp;Seek IT assistance if in doubt.&nbsp;&nbsp;</p>\n'''
 
-UNKNOWN_BANNER_PLAIN = '''Caution: The domain of email sender is first seen.  Beware of any
+UNKNOWN_BANNER_PLAIN = '''注意：這是首次接收到的電郵地址。除非您確保其真確性，否則請留意當中所附有的超連結，附件或銀行帳戶資料。如有疑問，請尋求技術人員的支援。
+Caution: The domain of email sender is first seen.  Beware of any
+hyperlink, attachment and bank account information unless you ensure the
+authenticity of the sender.  Seek IT assistance if in doubt.  
+
+'''
+
+JUNK_BANNER_PLAIN = '''警告：這是曾被舉報為垃圾發件人的電郵地址。除非您確保其真確性，否則請留意當中所附有的超連結，附件或銀行帳戶資料。如有疑問，請尋求技術人員的支援。
+Warning: The domain of email sender was reported as a junk sender.  Beware of any
+hyperlink, attachment and bank account information unless you ensure the
+authenticity of the sender.  Seek IT assistance if in doubt.  
+
+'''
+########
+# Current warnings
+# END
+########
+
+########
+# BEGIN
+# Warnings in previous version
+# This is required when updating the warning banners.
+# Otherwise, all emails are messed up.
+########
+PREV_UNKNOWN_SUBJECT = '''[FROM NEW SENDER] '''
+PREV_JUNK_SUBJECT = '''[JUNK MAIL] '''
+PREV_UNKNOWN_BANNER_HTML = '''<p style="margin: 10px 20%; text-align: center; border: 2px solid; background-color: #ff7400; padding: 5px;">Caution: The domain of email sender is first seen. &nbsp;Beware of any hyperlink, attachment and bank account information unless you ensure the authenticity of the sender. &nbsp;Seek IT assistance if in doubt.&nbsp;&nbsp;<br /><span>注意：這是首次接收到的電郵地址。除非您確保其真確性，否則請留意當中所附有的超連結，附件或銀行帳戶資料。如有疑問，請尋求技術人員的支援。</span></p>\n'''
+PREV_JUNK_BANNER_HTML = '''<p style="margin: 10px 20%; text-align: center; border: 2px solid; background-color: #ff4444; padding: 5px;">Warning: The domain of email sender was reported as a junk sender. &nbsp;Beware of any hyperlink, attachment and bank account information unless you ensure the authenticity of the sender. &nbsp;Seek IT assistance if in doubt.&nbsp;&nbsp;<br /><span>注意：這是曾被舉報為垃圾發件人的電郵地址。除非您確保其真確性，否則請留意當中所附有的超連結，附件或銀行帳戶資料。如有疑問，請尋求技術人員的支援。</span></p>\n'''
+
+PREV_UNKNOWN_BANNER_PLAIN = '''Caution: The domain of email sender is first seen.  Beware of any
 hyperlink, attachment and bank account information unless you ensure the
 authenticity of the sender.  Seek IT assistance if in doubt.  
 注意：這是首次接收到的電郵地址。除非您確保其真確性，否則請留意當中所附有的超連結，附件或銀行帳戶資料。如有疑問，請尋求技術人員的支援。
 
 '''
-UNKNOWN_BANNER_HTML = '''<p style="margin: 10px 20%; text-align: center; border: 2px solid; background-color: #ff7400; padding: 5px;">Caution: The domain of email sender is first seen. &nbsp;Beware of any hyperlink, attachment and bank account information unless you ensure the authenticity of the sender. &nbsp;Seek IT assistance if in doubt.&nbsp;&nbsp;<br /><span>注意：這是首次接收到的電郵地址。除非您確保其真確性，否則請留意當中所附有的超連結，附件或銀行帳戶資料。如有疑問，請尋求技術人員的支援。</span></p>\n'''
 
-JUNK_BANNER_PLAIN = '''Warning: The domain of email sender was reported as a junk sender.  Beware of any
+PREV_JUNK_BANNER_PLAIN = '''Warning: The domain of email sender was reported as a junk sender.  Beware of any
 hyperlink, attachment and bank account information unless you ensure the
 authenticity of the sender.  Seek IT assistance if in doubt.  
 警告：這是曾被舉報為垃圾發件人的電郵地址。除非您確保其真確性，否則請留意當中所附有的超連結，附件或銀行帳戶資料。如有疑問，請尋求技術人員的支援。
 
 '''
-JUNK_BANNER_HTML = '''<p style="margin: 10px 20%; text-align: center; border: 2px solid; background-color: #ff4444; padding: 5px;">Warning: The domain of email sender was reported as a junk sender. &nbsp;Beware of any hyperlink, attachment and bank account information unless you ensure the authenticity of the sender. &nbsp;Seek IT assistance if in doubt.&nbsp;&nbsp;<br /><span>注意：這是曾被舉報為垃圾發件人的電郵地址。除非您確保其真確性，否則請留意當中所附有的超連結，附件或銀行帳戶資料。如有疑問，請尋求技術人員的支援。</span></p>\n'''
-
+########
+# Warnings in previous version
+# END
+########
 
 def find_body_plain_and_html_from_message(msg):
     body_plain = ""
@@ -60,8 +95,10 @@ def find_body_plain_and_html_from_message(msg):
 def remove_banner_from_subject(filepath, is_junk=False):
     if is_junk:
         SUBJECT = JUNK_SUBJECT
+        PREV_SUBJECT = PREV_JUNK_SUBJECT
     else:
         SUBJECT = UNKNOWN_SUBJECT
+        PREV_SUBJECT = PREV_UNKNOWN_SUBJECT
 
     with open(filepath, "r+") as f:
         # Get msg and headers from file for further processing
@@ -70,8 +107,13 @@ def remove_banner_from_subject(filepath, is_junk=False):
         headers = parser.parsestr(msg.as_string())
 
         subject = headers['Subject']
-        if subject.startswith(SUBJECT):
-            headers.replace_header('Subject', subject.replace(SUBJECT,''))
+        if subject:
+            if subject.startswith(SUBJECT):
+                headers.replace_header('Subject', subject.replace(SUBJECT, ''))
+            # Start with subject of the previous version instead of the current version.
+            # That's the case subject has been updated and migration is required.
+            elif subject.startswith(PREV_SUBJECT):
+                headers.replace_header('Subject', subject.replace(PREV_SUBJECT, ''))
             f.seek(0)
             f.write(headers.as_string())
             f.truncate()
@@ -83,8 +125,10 @@ def remove_banner_from_subject(filepath, is_junk=False):
 def add_banner_to_subject(filepath, is_junk=False):
     if is_junk:
         SUBJECT = JUNK_SUBJECT
+        PREV_SUBJECT = PREV_JUNK_SUBJECT
     else:
         SUBJECT = UNKNOWN_SUBJECT
+        PREV_SUBJECT = PREV_UNKNOWN_SUBJECT
 
     with open(filepath, "r+") as f:
         # Get msg and headers from file for further processing
@@ -93,10 +137,22 @@ def add_banner_to_subject(filepath, is_junk=False):
         headers = parser.parsestr(msg.as_string())
 
         subject = headers['Subject']
-        if subject.startswith(SUBJECT):
-            pass
+
+        if subject:
+            if subject.startswith(SUBJECT):
+                pass
+            else:
+                # Start with subject of the previous version instead of the current version.
+                # That's the case subject has been updated and migration is required.
+                if subject.startswith(PREV_SUBJECT):
+                    headers.replace_header('Subject', subject.replace(PREV_SUBJECT, SUBJECT))
+                else:
+                    headers.replace_header('Subject', SUBJECT + subject)
+                f.seek(0)
+                f.write(headers.as_string())
+                f.truncate()
         else:
-            headers.replace_header('Subject', SUBJECT + subject)
+            headers.add_header('Subject', SUBJECT)
             f.seek(0)
             f.write(headers.as_string())
             f.truncate()
@@ -141,9 +197,13 @@ def remove_banner_from_body(filepath, is_junk=False):
     if is_junk:
         BANNER_PLAIN = JUNK_BANNER_PLAIN
         BANNER_HTML = JUNK_BANNER_HTML
+        PREV_BANNER_PLAIN = PREV_JUNK_BANNER_PLAIN
+        PREV_BANNER_HTML = PREV_JUNK_BANNER_HTML
     else:
         BANNER_PLAIN = UNKNOWN_BANNER_PLAIN
         BANNER_HTML = UNKNOWN_BANNER_HTML
+        PREV_BANNER_PLAIN = PREV_UNKNOWN_BANNER_PLAIN
+        PREV_BANNER_HTML = PREV_UNKNOWN_BANNER_HTML
 
     with open(filepath, "r+") as f:
         # Use policy=policy.default so that this returns an EmailMessage object instead of Message object.
@@ -169,12 +229,26 @@ def remove_banner_from_body(filepath, is_junk=False):
         body_plain = body_plain.decode("utf-8")
         body_html = body_html.decode("utf-8")
 
-        new_body_plain = re.sub(BANNER_PLAIN, '', body_plain)
-        new_body_html = re.sub(BANNER_HTML, '', body_html)
+        # The lines below remove existing banner(s) first.
+        #### BEGIN remove ####
+        if re.search(f'{BANNER_PLAIN}', body_plain):
+            body_plain = re.sub(BANNER_PLAIN, '', body_plain)
+        # Start with subject of the previous version instead of the current version.
+        # That's the case subject has been updated and migration is required.
+        elif re.search(f'{PREV_BANNER_PLAIN}', body_plain):
+            body_plain = re.sub(PREV_BANNER_PLAIN, '', body_plain)
+
+        if re.search(f'{BANNER_HTML}', body_html):
+            body_html = re.sub(BANNER_HTML, '', body_html)
+        # Start with subject of the previous version instead of the current version.
+        # That's the case subject has been updated and migration is required.
+        elif re.search(f'{PREV_BANNER_HTML}', body_html):
+            body_html = re.sub(PREV_BANNER_HTML, '', body_html)
+        #### END remove ####
 
         # Record the MIME types of both parts - text/plain and text/html.
-        part1 = MIMEText(new_body_plain, 'plain')
-        part2 = MIMEText(new_body_html, 'html')
+        part1 = MIMEText(body_plain, 'plain')
+        part2 = MIMEText(body_html, 'html')
 
         # Attach parts into message container.
         # According to RFC 2046, the last part of a multipart message, in this case
@@ -193,9 +267,13 @@ def add_banner_to_body(filepath, is_junk=False):
     if is_junk:
         BANNER_PLAIN = JUNK_BANNER_PLAIN
         BANNER_HTML = JUNK_BANNER_HTML
+        PREV_BANNER_PLAIN = PREV_JUNK_BANNER_PLAIN
+        PREV_BANNER_HTML = PREV_JUNK_BANNER_HTML
     else:
         BANNER_PLAIN = UNKNOWN_BANNER_PLAIN
         BANNER_HTML = UNKNOWN_BANNER_HTML
+        PREV_BANNER_PLAIN = PREV_UNKNOWN_BANNER_PLAIN
+        PREV_BANNER_HTML = PREV_UNKNOWN_BANNER_HTML
 
     with open(filepath, "r+") as f:
         # Use policy=policy.default so that this returns an EmailMessage object instead of Message object.
@@ -222,9 +300,22 @@ def add_banner_to_body(filepath, is_junk=False):
             body_plain = body_plain.decode("utf-8")
             body_html = body_html.decode("utf-8")
 
-            # The 2 lines below remove existing banner(s) first.
-            body_plain = re.sub(BANNER_PLAIN, '', body_plain)
-            body_html = re.sub(BANNER_HTML, '', body_html)
+            # The lines below remove existing banner(s) first.
+            #### BEGIN remove ####
+            if re.search(f'{BANNER_PLAIN}', body_plain):
+                body_plain = re.sub(BANNER_PLAIN, '', body_plain)
+            # Start with subject of the previous version instead of the current version.
+            # That's the case subject has been updated and migration is required.
+            elif re.search(f'{PREV_BANNER_PLAIN}', body_plain):
+                body_plain = re.sub(PREV_BANNER_PLAIN, '', body_plain)
+
+            if re.search(f'{BANNER_HTML}', body_html):
+                body_html = re.sub(BANNER_HTML, '', body_html)
+            # Start with subject of the previous version instead of the current version.
+            # That's the case subject has been updated and migration is required.
+            elif re.search(f'{PREV_BANNER_HTML}', body_html):
+                body_html = re.sub(PREV_BANNER_HTML, '', body_html)
+            #### END remove ####
 
             html = f'''<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /></head><body style=\'font-size: 10pt; font-family: Verdana,Geneva,sans-serif\'>\n{BANNER_HTML}{body_html}</body></html>\n'''
         else:
